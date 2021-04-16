3847941a082fa41835dc580e825dc120760ac849
Ben <ben@bengehring.com>
2021-04-16 16:48:37 -0400

    added persistance class.

---
+++ Directory/data/DirectoryData.cs
@@ -0,0 +1,31 @@
+﻿using domain;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace data
+{
+    public class DirectoryData : IDirectoryData<Business>
+    {
+        private DbContext _context;
+        private readonly DbSet<Business> _dbSet;
+
+        public DirectoryData(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<Business>();
+        }
+
+        public IEnumerable<Business> GetBusinesses()
+        {
+            return _dbSet.ToList();
+        }
+
+        public IEnumerable<Business> FindBusinesses(string searchTerm)
+        {
+            return _dbSet.Where(b =>
+                b.BusinessName.Contains(searchTerm) | b.City.Contains(searchTerm) | b.State.Contains(searchTerm) |
+                b.BusinessType.BusinessTypeName.Contains(searchTerm));
+        }
+    }
+}
---
+++ Directory/data/IDirectoryData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace data
+{
+    public interface IDirectoryData<out TEntity> where TEntity : class
+    {
+        public IEnumerable<TEntity> GetBusinesses();
+
+        public IEnumerable<TEntity> FindBusinesses(string searchTerm);
+    }
+}
